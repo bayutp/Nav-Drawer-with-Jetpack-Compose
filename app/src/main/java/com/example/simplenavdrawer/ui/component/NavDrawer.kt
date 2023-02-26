38a38b9796a5abd714ac4aa7977044aa687a05df
bayupamuji <bayuteguhpamuji@gmail.com>
2023-02-26 13:47:55 +0700

    add snackbar/toast & back press

--- app/src/main/java/com/example/simplenavdrawer/ui/component/NavDrawer.kt
+++ app/src/main/java/com/example/simplenavdrawer/ui/component/NavDrawer.kt
@@ -1,5 +1,9 @@
 package com.example.simplenavdrawer.ui.component
 
+import android.app.Activity
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,12 +13,13 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +32,7 @@ import kotlinx.coroutines.launch
 fun NavDrawer() {
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -36,7 +42,30 @@ fun NavDrawer() {
             })
         },
         drawerContent = {
-            ContentDrawer(onItemSelected = {})
+            ContentDrawer(onItemSelected = { title ->
+                scope.launch {
+                    scaffoldState.drawerState.close()
+                    val snackbarResult = scaffoldState.snackbarHostState.showSnackbar(
+                        message = context.resources.getString(
+                            R.string.coming_soon,
+                            title
+                        ), actionLabel = context.resources.getString(R.string.subscribe_question)
+                    )
+                    if (snackbarResult == SnackbarResult.ActionPerformed) {
+                        Toast.makeText(
+                            context,
+                            context.resources.getString(R.string.subscribed_info),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }, onBackPress = {
+                if (scaffoldState.drawerState.isOpen) {
+                    scope.launch {
+                        scaffoldState.drawerState.close()
+                    }
+                }else (context as Activity).finish()
+            })
         },
         drawerGesturesEnabled = scaffoldState.drawerState.isOpen
     ) { paddingValues ->
@@ -63,7 +92,11 @@ fun MyTopBar(onMenuClick: () -> Unit) {
 }
 
 @Composable
-fun ContentDrawer(modifier: Modifier = Modifier, onItemSelected: (title: String) -> Unit) {
+fun ContentDrawer(
+    modifier: Modifier = Modifier,
+    onItemSelected: (title: String) -> Unit,
+    onBackPress: () -> Unit
+) {
     val items = listOf(
         MenuItem(title = stringResource(id = R.string.home), icon = Icons.Default.Home),
         MenuItem(title = stringResource(id = R.string.favourite), icon = Icons.Default.Favorite),
@@ -97,6 +130,34 @@ fun ContentDrawer(modifier: Modifier = Modifier, onItemSelected: (title: String)
             }
         }
         Divider()
+    }
+    BackPressHandler {
+        onBackPress()
+    }
+}
+
+@Composable
+fun BackPressHandler(enabled: Boolean = true, onBackPressed: () -> Unit) {
+    val currentOnBackPressed by rememberUpdatedState(onBackPressed)
+    val backCallback = remember {
+        object : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+                currentOnBackPressed()
+            }
+        }
+    }
+    SideEffect {
+        backCallback.isEnabled = enabled
+    }
+    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
+        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
+    }.onBackPressedDispatcher
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, backCallback)
+        onDispose {
+            backCallback.remove()
+        }
     }
 }
 
